a0abf2f8cbdf75466fab46b2313c14db8062687a
Sammels <Sasagete@gmail.com>
2023-05-11 17:48:56 +0300

    Added calculate median algorithm salary

--- language-salary.py
+++ language-salary.py
@@ -1,9 +1,8 @@
 import time
 import requests
+import logging
 
-
-if __name__ == "__main__":
-    start_time = time.time()
+def parse_hh():
     programm_language_popular = {}
     programm_languages = ["Python", "Java", "Javascript", "Ruby",
                           "PHP", "C++", "C#", "C",
@@ -19,7 +18,7 @@ if __name__ == "__main__":
         payload = {
             'area': '1',
             'text': f"Программист {language}",
-            'period': 3,
+            'period': 30,
             'only_with_salary': True,
         }
 
@@ -28,6 +27,8 @@ if __name__ == "__main__":
         response.raise_for_status()
         cont = response.json().get('items')
         found_vacancies = response.json().get('found')
+        salary = cont[0]['salary']
+        print(language, salary)
 
         # # Parse work title
         # vacansies_list = []
@@ -38,6 +39,79 @@ if __name__ == "__main__":
         programm_language_popular[language] = found_vacancies
 
     print(programm_language_popular)
+
+
+def predict_rub_salary(vacancies_id: str) -> str:
+    """The function predicts the salary in rubles"""
+    vacancies_url_api = f'https://api.hh.ru/vacancies/{vacancies_id}'
+
+    headers = {
+        'User-Agent': 'api-test-agent'
+    }
+
+    payload = {
+        'area': '1',
+        'period': 3,
+        'only_with_salary': True,
+        'currency': 'RUR',
+    }
+    response = requests.get(vacancies_url_api, headers=headers, params=payload)
+    response.raise_for_status()
+    container = response.json().get("salary")
+
+    if 'USD' not in container['currency']:
+        if not container['from']:
+            max_salary = container['to'] * 0.8
+            print(max_salary)
+        elif not container['to']:
+            min_salary = container['from'] * 1.2
+            print(min_salary)
+        else:
+            min_salary = container['from']
+            max_salary = container['to']
+            current_salary = (min_salary + max_salary) // 2
+            print(current_salary)
+    else:
+        print(None)
+
+
+
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    vacancie_url_api = 'https://api.hh.ru/vacancies'
+
+    headers = {
+        'User-Agent': 'api-test-agent'
+    }
+
+    payload = {
+        'area': '1',
+        'per_page': 20,
+        'text': "Программист Python",
+        'period': 3,
+        'only_with_salary': True,
+        'currency': 'RUR',
+    }
+
+    response = requests.get(vacancie_url_api, headers=headers, params=payload)
+    response.raise_for_status()
+    cont = response.json().get('items')
+
+
+    vacansies_list = []
+    for number, items in enumerate(cont):
+        vacancies_id = items['id']
+        salary = items['salary']
+
+        predict_rub_salary(vacancies_id)
+
+        # print(number, items.get('name'),
+        #       "Salary: ", salary,
+        #       vacancies_id)
+
 
     # Check time resource
     end_time = time.time() - start_time
