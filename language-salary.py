6fdf3a53e351f6e079fdc18bce833e95cc32fe9d
Sammels <Sasagete@gmail.com>
2023-05-16 18:06:23 +0300

    Update language-salary.py

--- language-salary.py
+++ language-salary.py
@@ -2,45 +2,7 @@ import time
 import requests
 
 
-def parse_hh():
-    programm_language_popular = {}
-    programm_languages = ["Python", "Java", "Javascript", "Ruby",
-                          "PHP", "C++", "C#", "C",
-                          "Go", "Shell"]
-
-    vacancie_url_api = 'https://api.hh.ru/vacancies'
-    headers = {
-        'User-Agent': 'api-test-agent'
-    }
-
-    for language in programm_languages:
-        payload = {
-            'area': '1',
-            'text': f"Программист {language}",
-            'period': 30,
-            'only_with_salary': True,
-        }
-
-        # Vacansies in Moskow
-        response = requests.get(vacancie_url_api, headers=headers, params=payload)
-        response.raise_for_status()
-        cont = response.json().get('items')
-        found_vacancies = response.json().get('found')
-        salary = cont[0]['salary']
-        print(language, salary)
-
-        # # Parse work title
-        # vacansies_list = []
-        # for number, items in enumerate(cont):
-        #     print(number, items.get('name'), f"city: {items.get('area')['name']}")
-
-        # Шаг 3 словарь с языками
-        programm_language_popular[language] = found_vacancies
-
-    print(programm_language_popular)
-
-
-def predict_rub_salary(vacancies_id: str) -> str:
+def predict_rub_salary(vacancies_id: str) -> int:
     """The function predicts the salary in rubles"""
     vacancies_url_api = f'https://api.hh.ru/vacancies/{vacancies_id}'
 
@@ -59,22 +21,26 @@ def predict_rub_salary(vacancies_id: str) -> str:
 
     if 'USD' not in container['currency']:
         if not container['from']:
-            max_salary = container['to'] * 0.8
-            print(int(max_salary))
+            max_salary = int(container['to'] * 0.8)
+            return max_salary
         elif not container['to']:
-            min_salary = container['from'] * 1.2
-            print(int(min_salary))
+            min_salary = int(container['from'] * 1.2)
+            return min_salary
         else:
             min_salary = container['from']
             max_salary = container['to']
             current_salary = (min_salary + max_salary) // 2
-            print(current_salary)
+            return current_salary
     else:
-        return 'None'
+        return None
 
 
 if __name__ == "__main__":
     start_time = time.time()
+
+    programm_language_popular = {}
+    average_salary = []
+    vacancies_processed = []
 
     programm_languages = ["Python", "Java", "Javascript"]
 
@@ -87,7 +53,6 @@ if __name__ == "__main__":
     for language in programm_languages:
         payload = {
             'area': '1',
-            'per_page': 20,
             'text': f"Программист {language}",
             'period': 30,
             'only_with_salary': True,
@@ -97,12 +62,22 @@ if __name__ == "__main__":
         response.raise_for_status()
         cont = response.json().get('items')
         found_vacancies = response.json().get('found')
-        print(found_vacancies)
+
 
         for number, items in enumerate(cont):
+            programm_language = {}
             vacancies_id = items['id']
-            salary = items['salary']
-            predict_rub_salary(vacancies_id)
+            prediction_salary = predict_rub_salary(vacancies_id)
+            average_salary.append(prediction_salary)
+
+            vacancies_processed.append(number)
+            programm_language['found_vacancies'] = found_vacancies
+            programm_language['vacancies_processed'] = len(vacancies_processed)
+
+        vacancies_processed = []
+        programm_language_popular[language] = programm_language
+        #print(sum(average_salary) // len(average_salary))
+        print(programm_language_popular)
 
         time.sleep(8)
     # Check time resource
