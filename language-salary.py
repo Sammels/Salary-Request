42382ab4517aef76ed62a06248b1068e86c53e86
Sammels <Sasagete@gmail.com>
2023-05-31 19:29:38 +0300

    Update language-salary.py

--- language-salary.py
+++ language-salary.py
@@ -1,3 +1,4 @@
+import os
 import time
 import requests
 from dotenv import load_dotenv
@@ -33,8 +34,24 @@ def get_hh_vacancies(language: list[str]) -> list[str]:
     return hh_vacancies
 
 
-def get_sj_vacancies():
-    pass
+def get_sj_vacancies(language: str, token: str) -> list:
+    """Function get request to Superjob Api"""
+    sjob_api = 'https://api.superjob.ru/2.0/vacancies/'
+    header = {'X-Api-App-Id': token}
+    sjob_vacancies = []
+    for page in count(0):
+        profession_category = {'33': ['36', '48', '604']}
+        payload = {'town': '4', 'categories': profession_category,
+                   'count': 100, 'page': page,
+                   'keyword': language, 'period': 30}
+        response = requests.get(sjob_api, headers=header, params=payload)
+        response.raise_for_status()
+        vacancies = response.json()
+        sjob_vacancies += vacancies['objects']
+        if not vacancies['more']:
+            break
+    return sjob_vacancies
+
 
 def extract_hh_salary(vacancies) -> list[int]:
     """get vacancies list and return string"""
@@ -48,8 +65,16 @@ def extract_hh_salary(vacancies) -> list[int]:
         salaries.append(salary)
     return salaries
 
-def extract_sj_vacansies(vacancies):
-    pass
+def extract_sj_salary(vacancies) -> list[int]:
+    """get vacancies list and return string"""
+    salaries = []
+    for vacancy in vacancies:
+        salary = predict_rub_salary_for_sj(vacancy)
+        if not salary:
+            continue
+        salaries.append(salary)
+    return salaries
+
 
 
 def calculate_average_salary(salary_from: int, salary_to: int) -> int:
@@ -71,8 +96,11 @@ def predict_rub_salary_for_hh(salary: dict) -> int:
         average_salary = calculate_average_salary(salary["from"], salary["to"])
     return average_salary
 
-def predict_rub_salary_for_sj(salary: dict) -> int:
-    pass
+def predict_rub_salary_for_sj(vacancy: dict) -> int:
+    average_salary = 0
+    if vacancy["currency"] == "rub":
+        average_salary = calculate_average_salary(vacancy["payment_from"], vacancy["paymen_to"])
+    return average_salary
 
 def create_table(salary_by_language):
     table_salaries = [
@@ -104,13 +132,36 @@ def combine_hh_salaries():
     return salary_by_languages
 
 
+def combine_sj_vacancies():
+    salary_by_languages = {}
+    for language in PROGRAMM_LANGUAGE:
+        response = get_sj_vacancies(language, token)
+        salaries = extract_sj_salary(response)
+        reps_counts = len(response)
+        try:
+            average_salary = int(sum(salaries) / len(salaries))
+        except ZeroDivisionError:
+            average_salary = 0
+        salary_by_languages[language] = {'vacancies_found': reps_counts,
+                                         'vacancies_processed': len(salaries),
+                                         'average_salary': average_salary}
+    return salary_by_languages
+
+
 if __name__ == "__main__":
+    load_dotenv()
+
     start_time = time.time()
+    token = os.environ['SJ_TOKEN']
 
     table = create_table(combine_hh_salaries())
     table.title = 'HeadHunter Moscow'
     print(table.table)
     print()
+
+    table = create_table(combine_sj_vacancies())
+    table.title = "SuperJob Moskow"
+    print(table.table)
 
     # Check time resource
     end_time = time.time() - start_time
